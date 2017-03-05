7a29e5714527329bb74430915d916c88e7ef19f7
Edaena <edaena@gmail.com>
2017-03-04 17:30:39 -0800

    Add test data and read train csv in python

--- prices.py
+++ prices.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+
+train = pd.read_csv("train.csv", header=0, delimiter=",")
+# print train: prints the entire dataframe
+# train.head() prints the first 5 lines
+# train.shape
+# train MSSubClass
+# train.columns.valuest
+
+# prepare the data with only the relevant columns
+# a key component of pandas is the dataframe
+
+
 
